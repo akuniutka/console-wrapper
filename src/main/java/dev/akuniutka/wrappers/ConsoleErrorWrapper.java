b73a7e7bdbf33d2eed3b4e6406da8e2e4fd887cb
Andrei Kuniutka <akuniutka@gmail.com>
2023-10-15 17:32:36 +0300

    drafted wrapper classes

--- src/main/java/dev/akuniutka/wrappers/ConsoleErrorWrapper.java
+++ src/main/java/dev/akuniutka/wrappers/ConsoleErrorWrapper.java
@@ -1,4 +1,23 @@
 package dev.akuniutka.wrappers;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class ConsoleErrorWrapper {
+    private ByteArrayOutputStream wrapperStream;
+    private PrintStream systemErrorStream;
+
+    public void start() {
+        systemErrorStream = System.err;
+        wrapperStream = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(wrapperStream));
+    }
+
+    public String getData() {
+        return wrapperStream.toString();
+    }
+
+    public void stop() {
+        System.setErr(systemErrorStream);
+    }
 }
--- src/main/java/dev/akuniutka/wrappers/ConsoleOutputWrapper.java
+++ src/main/java/dev/akuniutka/wrappers/ConsoleOutputWrapper.java
@@ -1,4 +1,23 @@
 package dev.akuniutka.wrappers;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class ConsoleOutputWrapper {
+    private ByteArrayOutputStream wrapperOutputStream;
+    private PrintStream systemOutputStream;
+
+    public void start() {
+        systemOutputStream = System.out;
+        wrapperOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(wrapperOutputStream));
+    }
+
+    public String getData() {
+        return wrapperOutputStream.toString();
+    }
+
+    public void stop() {
+        System.setOut(systemOutputStream);
+    }
 }
